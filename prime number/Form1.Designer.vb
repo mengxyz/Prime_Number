573b80ccb8e383c0387452d5b8d689f077f9e846
mengxyz <36928202+mengxyz@users.noreply.github.com>
2019-01-23 10:51:56 +0700

    final

---
+++ prime number/Form1.Designer.vb
@@ -0,0 +1,75 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.txtInput = New System.Windows.Forms.TextBox()
+        Me.btnOK = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'txtInput
+        '
+        Me.txtInput.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtInput.Location = New System.Drawing.Point(117, 30)
+        Me.txtInput.Name = "txtInput"
+        Me.txtInput.Size = New System.Drawing.Size(119, 31)
+        Me.txtInput.TabIndex = 0
+        '
+        'btnOK
+        '
+        Me.btnOK.Location = New System.Drawing.Point(117, 120)
+        Me.btnOK.Name = "btnOK"
+        Me.btnOK.Size = New System.Drawing.Size(100, 23)
+        Me.btnOK.TabIndex = 2
+        Me.btnOK.Text = "OK"
+        Me.btnOK.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(12, 34)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(78, 24)
+        Me.Label1.TabIndex = 3
+        Me.Label1.Text = "ใส่จำนวน"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(284, 182)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.btnOK)
+        Me.Controls.Add(Me.txtInput)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents txtInput As System.Windows.Forms.TextBox
+    Friend WithEvents btnOK As System.Windows.Forms.Button
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+
+End Class
---
+++ prime number/Form1.vb
@@ -0,0 +1,34 @@
+﻿Public Class Form1
+
+    Sub CheckPrimeNumber(ByVal n As Integer)
+        Dim isPrime As Boolean = True
+
+        If (n <= 1) Then
+            MsgBox("ไม่เป็นจำนวนเฉพาะ")
+            Exit Sub
+        End If
+
+        If (n Mod 2 = 0 And n <> 2) Then
+            isPrime = True
+        ElseIf (n Mod 3 = 0 And n <> 3) Then
+            isPrime = True
+        ElseIf (n Mod 7 = 0 And n <> 7) Then
+            isPrime = True
+        ElseIf (n Mod 5 = 0 And n <> 5) Then
+            isPrime = True
+        Else
+            isPrime = False
+        End If
+
+
+        If isPrime = False Then
+            MsgBox("เป็นจำนวนเฉพาะ")
+        Else
+            MsgBox("ไม่เป็นจำนวนเฉพาะ")
+        End If
+    End Sub
+
+    Private Sub btnOK_Click(sender As Object, e As EventArgs) Handles btnOK.Click
+        CheckPrimeNumber(txtInput.Text) 'ส่งตัวเลขจาก text box ไปฟังชั่น
+    End Sub
+End Class
